549db73d2d21b9c794ee2c5e4f3599bc0e48f8b7
Mike Samuel <mikesamuel@gmail.com>
2018-09-16 14:57:12 -0400

    Incorporate non-Closure version of HTML type contracts
    This provides a source of truth per issue #1.

---
+++ test/contracts-test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const { expect } = require('chai');
+const { describe, it } = require('mocha');
+
+const contracts = require('../lib/contracts/contracts.js');
+
+describe('contracts', () => {
+  it('contentTypeForElement', () => {
+    // 3 means SCRIPT
+    // eslint-disable-next-line no-magic-numbers
+    expect(contracts.contentTypeForElement('script')).to.equal(3);
+  });
+  it('typeOfAttribute', () => {
+    expect(contracts.typeOfAttribute(
+      'script', 'src',
+      // Getter for value of other attributes where known.
+      (x) => (x === 'type' ? 'javascript' : null)))
+      // 4 means TRUSTED_RESOURCE_URL
+      // eslint-disable-next-line no-magic-numbers
+      .to.equal(4);
+  });
+});
