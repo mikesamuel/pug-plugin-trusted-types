2b19082f6f208ef6ebc1c2d9bec6b1f23cfeb306
Mike Samuel <mikesamuel@gmail.com>
2018-09-23 16:00:41 -0400

    debugging scrubbers

--- packages/scrubber/index.js
+++ packages/scrubber/index.js
@@ -14,23 +14,22 @@ const { create, hasOwnProperty } = Object;
 const { apply } = Reflect;
 
 const {
-  trustedHTMLGuard,
-  trustedScriptGuard,
   GUARDS_BY_ATTRIBUTE_TYPE,
 } = require('pug-guards-trusted-types');
 
 const GUARDS = {
   __proto__: null,
-  'reject': reject,
-  'requireTrustedHTML': requireTrustedHTML,
-  'requireTrustedResourceURL': requireTrustedResourceURL,
-  'requireTrustedScript': requireTrustedScript,
-  'requireTrustedURL': requireTrustedURL,
+  reject,
+  requireTrustedHTML,
+  requireTrustedResourceURL,
+  requireTrustedScript,
+  requireTrustedURL,
 };
 
 function scrubAttrs(elementName, attrs) {
   const outputAttrs = create(null);
-  for (const attrName in attrs) {  // Mimics loop in pug.attrs
+  // This loop mimics a loop in pug.attrs().
+  for (const attrName in attrs) {
     if (apply(hasOwnProperty, attrs, [ attrName ])) {
       // We have to copy value over to avoid polymorphic input attacks.
       if (attrName !== '__proto__') {
@@ -42,11 +41,11 @@ function scrubAttrs(elementName, attrs) {
     return `${ outputAttrs[attrName] }`;
   }
   for (const attrName in outputAttrs) {
-    const value = outputAttrs[key];
+    const value = outputAttrs[attrName];
     const type = typeOfAttribute(elementName, attrName, getValue);
     const guard = GUARDS_BY_ATTRIBUTE_TYPE[type];
     if (guard) {
-      let guardFn = GUARDS[guard];
+      const guardFn = GUARDS[guard];
       outputAttrs[attrName] = guardFn(value);
     }
   }
